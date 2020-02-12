6509cd188d6f618afbd20673cf308ae167bf48bc
Timur Khaimovich <itima.com@gmail.com>
2020-02-12 09:41:41 +0600

    Very Beautiful Laliexpress without cool features

--- w4/src/app/app.module.ts
+++ w4/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './product-list/product-list.component';
 import {RouterModule} from '@angular/router';
-import { SuperHeaderComponent } from './super-header/super-header.component';
-import {ShareButtonModule} from '@ngx-share/button';
+import {SuperHeaderComponent} from './super-header/super-header.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ShareButtonsModule} from '@ngx-share/buttons';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,12 +17,14 @@ import {ShareButtonsModule} from '@ngx-share/buttons';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([{path: 'products', component: ProductListComponent}]),
     HttpClientModule,
-    ShareButtonModule,
-    ShareButtonsModule
+    ShareButtonsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- w4/src/app/product-list/product-list.component.ts
+++ w4/src/app/product-list/product-list.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {products} from '../products';
-import { ShareButtonModule} from '@ngx-share/button';
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent implements OnInit {
   products = products;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
