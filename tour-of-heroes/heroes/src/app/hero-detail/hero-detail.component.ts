554556b2ec2615939ea293780aa44f1b94f03127
Timur Khaimovich <itima.com@gmail.com>
2020-02-29 14:53:14 +0600

    Reupload idk why

---
+++ tour-of-heroes/heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {Input} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+}
